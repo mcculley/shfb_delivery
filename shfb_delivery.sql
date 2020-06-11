8f789fe55e25fb845c33e8cff9f195f1fe5681ae
Gene McCulley <mcculley@stackframe.com>
2020-06-11 12:03:30 -0400

    Change customer to client.

--- shfb_delivery.sql
+++ shfb_delivery.sql
@@ -9,17 +9,17 @@ create table staff_member
     name_last  text   not null
 );
 
-comment on table staff_member is 'A member of the SHFB staff responsible for triaging and updating a customer.';
+comment on table staff_member is 'A member of the SHFB staff responsible for triaging and updating a client.';
 
 comment on column staff_member.id is 'The unique ID of the staff member.';
 
 create unique index staff_member_id_uindex
     on staff_member (id);
 
-create table customer
+create table client
 (
 	id serial not null
-		constraint customer_pk
+		constraint client_pk
 			primary key,
 	name_first text not null,
 	name_last text not null,
@@ -48,27 +48,27 @@ create table customer
 	note text
 );
 
-create unique index customer_id_uindex
-	on customer (id);
+create unique index client_id_uindex
+	on client (id);
 
-comment on table customer is 'A household requesting food delivery.';
+comment on table client is 'A household requesting food delivery.';
 
-comment on column customer.adults is 'The number of non-senior adults in the home.';
+comment on column client.adults is 'The number of non-senior adults in the home.';
 
-comment on column customer.seniors is 'The number of senior citizens in the home.';
+comment on column client.seniors is 'The number of senior citizens in the home.';
 
-comment on column customer.children is 'The number of children in the home.';
+comment on column client.children is 'The number of children in the home.';
 
-comment on column customer.family_size is 'The total number of people in the home (computed from the total number of children, adults, and seniors).';
+comment on column client.family_size is 'The total number of people in the home (computed from the total number of children, adults, and seniors).';
 
-comment on column customer.note is 'Free form text about the customer.';
+comment on column client.note is 'Free form text about the client.';
 
 create table eligibility_determination
 (
 	timestamp timestamp not null,
-	customer integer not null
-		constraint eligibility_determination_customer_id_fk
-			references customer
+	client integer not null
+		constraint eligibility_determination_client_id_fk
+			references client
 				on delete restrict,
 	eligible boolean not null,
 	note text,
@@ -79,20 +79,20 @@ create table eligibility_determination
 
 );
 
-comment on table eligibility_determination is 'A log of eligibility determinations for a customer. If a customer does not appear in this table, it needs to be triaged.';
+comment on table eligibility_determination is 'A log of eligibility determinations for a client. If a client does not appear in this table, it needs to be triaged.';
 
 comment on column eligibility_determination.timestamp is 'The timestamp of the determination.';
 
 comment on column eligibility_determination.note is 'A free form note optionally entered by the staff member.';
 
-create table customer_update
+create table client_update
 (
-    customer  integer   not null
-        constraint customer_update_customer_id_fk
-            references customer
+    client  integer   not null
+        constraint client_update_client_id_fk
+            references client
             on delete restrict,
     editor    integer   not null
-        constraint customer_update_staff_member_id_fk
+        constraint client_update_staff_member_id_fk
             references staff_member
             on delete restrict,
     timestamp timestamp not null,
@@ -101,9 +101,9 @@ create table customer_update
 
 create table delivery
 (
-    customer  integer   not null
-        constraint delivery_customer_id_fk
-            references customer
+    client  integer   not null
+        constraint delivery_client_id_fk
+            references client
             on delete restrict,
     timestamp timestamp not null,
     note      text
